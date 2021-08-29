34c452cd7a86316fafb1f70152f91d2214df099f
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2021-08-29 19:08:01 -0400

    building out some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :pos
+  resources :pos do 
+    resources :statements
+    resources :participants
+    resources :installements
+    resources :line_items
+  end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

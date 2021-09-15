81e72cc8ae1c94d124ba39736c494239aa7bdbda
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2021-09-15 14:40:35 -0400

    Adding flash message for intallments falure.

--- app/reflexes/installment_reflex.rb
+++ app/reflexes/installment_reflex.rb
@@ -29,24 +29,26 @@ class InstallmentReflex < ApplicationReflex
       po.save
       flash.alert = 'Installments adjusted please add due dates'
     elsif (! check_string(installments)) && percentage_ceiling(installments)
-        po.installments.destroy_all
-        date_collector = []
-        multiple_installments = installments.split(" ")
-        num = (multiple_installments.length + 1) -2
-        date = ((po.end_date.to_i - po.start_date.to_i) / num).round.abs
-        multiple_installments.each_with_index do |installment, i| 
-          if i == 0
-            portion = Installment.new(po_id: po.id, percentage: installment, due_date: po.start_date)
-          else
-            new_due_date = date_collector[-1] + date
-            portion = Installment.new(po_id: po.id, percentage: installment, due_date: new_due_date)
-          end
-            portion.save 
-            date_collector << portion.due_date
-          end
-        po.number_of_installments = date_collector.length
-        po.save
-        flash.alert = 'Installments adjusted please add due dates'
+      po.installments.destroy_all
+      date_collector = []
+      multiple_installments = installments.split(" ")
+      num = (multiple_installments.length + 1) -2
+      date = ((po.end_date.to_i - po.start_date.to_i) / num).round.abs
+      multiple_installments.each_with_index do |installment, i| 
+        if i == 0
+          portion = Installment.new(po_id: po.id, percentage: installment, due_date: po.start_date)
+        else
+          new_due_date = date_collector[-1] + date
+          portion = Installment.new(po_id: po.id, percentage: installment, due_date: new_due_date)
+        end
+          portion.save 
+          date_collector << portion.due_date
+        end
+      po.number_of_installments = date_collector.length
+      po.save
+      flash.alert = 'Installments adjusted please add due dates'
+    else
+      flash.alert = 'Installment could not be saved. Please refer to the pattern below the form. Remember that decimals are not accepted and that the total of the installments must equal 100.'
     end
   end
 end

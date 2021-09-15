9a2a74e21ff406b707898e280f70067434272c2b
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2021-09-15 14:44:20 -0400

    Adding password_complexity validation to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,16 @@ class User < ApplicationRecord
   encrypts :email
   blind_index :email
 
+  validate :password_complexity
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :registerable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :validatable
 
+  def password_complexity
+    return if password.blank? || password =~ /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/
+    errors.add :password, 'Please use at least: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+  end
+      
 end

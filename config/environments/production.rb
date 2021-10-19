5165d2714070f1f152328234f3e817c3d6e76bdb
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2021-10-19 18:57:43 -0400

    Adding setting up heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,17 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.force_ssl = true
+
+ ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: 'apikey',
+    password: ENV['SENDGRID_API_KEY'],
+    domain: 'heroku.com', 
+    enable_starttls_auto: true
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.

5016cef6851c4a14ff492bb946faadd344a16e51
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2021-10-20 22:13:03 -0400

    working on pdf attachment error

--- app/mailers/statement_mailer.rb
+++ app/mailers/statement_mailer.rb
@@ -1,7 +1,7 @@
 class StatementMailer < ApplicationMailer
 
     def pdf_attachment_method(statement)
-        attachments["invoice_#{statement.invoice_number}.pdf"] =   WickedPdf.new.pdf_from_string(render_to_string(template: 'statements/show.html.erb', layout: 'statement.html.erb', pdf: 'filename'))
+        attachments["statement_#{statement.id}.pdf"] =   WickedPdf.new.pdf_from_string(render_to_string(template: 'statements/show.html.erb', layout: 'statement.html.erb', pdf: 'filename'))
         mail(to: 'info@bonarinstitute.com', subject: 'Your PDF is attached', statement: statement)
     end
 end

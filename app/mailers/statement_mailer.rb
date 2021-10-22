167a261579c5d0d6664e6245b9174bfeb41e9c71
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2021-10-22 15:31:33 -0400

    Working on mailer

--- app/mailers/statement_mailer.rb
+++ app/mailers/statement_mailer.rb
@@ -1,8 +1,6 @@
 class StatementMailer < ApplicationMailer
-
-    def pdf_attachment(self)
-        statement = Statement.last
-        attachments["statement_#{statement.id}.pdf"] = WickedPdf.new.pdf_from_string(
+    def pdf_attachment(statement)
+        attachments["statement_#{statement.po_number}.pdf"] = WickedPdf.new.pdf_from_string(
             render_to_string(template: 'statements/show.html.erb', layout: 'statement.html.erb', pdf: 'filename')
             )
         mail(to: 'info@bonarinstitute.com', subject: 'Your PDF is attached')

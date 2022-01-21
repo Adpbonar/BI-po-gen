15fcf8b9ed2932b44cfd85dcb16f392e4e12d1ba
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2022-01-21 13:38:21 -0500

    Working on PDF

--- app/mailers/statement_mailer.rb
+++ app/mailers/statement_mailer.rb
@@ -9,7 +9,7 @@ class StatementMailer < ApplicationMailer
 
     def pdf_attachment(statement)
         @statement = statement
-        attachments["statement_#{statement.po_number}.pdf"] = WickedPdf.new.pdf_from_string(
+        attachments["invoice_#{statement.invoice_number}.pdf"] = WickedPdf.new.pdf_from_string(
                 render_to_string(template: 'statements/show.html.erb', 
                     layout: 'statement.html.erb', 
                     pdf: 'filename', 

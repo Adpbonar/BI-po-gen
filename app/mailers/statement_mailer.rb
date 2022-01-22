91569c555410901c3f9604ae097e6a3b9e72cc29
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2022-01-22 11:53:16 -0500

    Working on emails

--- app/mailers/statement_mailer.rb
+++ app/mailers/statement_mailer.rb
@@ -9,7 +9,7 @@ class StatementMailer < ApplicationMailer
 
     def pdf_attachment(statement)
         @statement = statement
-        @greeting = "Hello #{@statement.tatement.participant_name},<br /> You have a new #{@statement.kind?} attached to this message. If you have any questions or concerns, please don\'t hesitate to reach out. <br />Kindest regards,<br />The team at the Bonar Institute for Purposeful Leadership."
+        @greeting = "Hello #{@statement.participant_name},<br /> You have a new #{@statement.kind?} attached to this message. If you have any questions or concerns, please don\'t hesitate to reach out. <br />Kindest regards,<br />The team at the Bonar Institute for Purposeful Leadership."
         attachments["Bonar Institute for Purposeful Leadership - #{statement.kind?}_#{statement.invoice_number}.pdf"] = WickedPdf.new.pdf_from_string(
                 render_to_string(template: 'statements/show.html.erb', 
                     layout: 'statement.html.erb', 

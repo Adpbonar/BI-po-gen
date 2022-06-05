5373e2e9f54147e8cd4d2fdf3ae1388877be5135
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2022-06-05 11:17:34 -0400

    Formatting Fixed Payment PO and adding removal of rates

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   resources :statements, except: [ :destroy ] 
   resources :participants
   resources :line_items, only: [ :new, :create, :edit, :update, :destroy ]
-  resources :items, except: [ :index, :show ]
+  resources :items, except: [ :index, :show, :edit, :update ]
   resources :ranking_forms, path: 'forms', except: [ :destroy, :edit ]
   resources :rates, only: [ :new, :create, :update ]
   resources :discounts, only: [ :new, :create, :destroy ]
